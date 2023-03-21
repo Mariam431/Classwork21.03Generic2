6860803d789f7267e915698e3d0f5bcabdf70899
mariam <mariam@MYPROBOOK>
2023-03-21 21:42:33 +0400

    Add project files.

---
+++ Classwork21.03Generic2/Generic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork21._03Generic2
+{
+    internal class Generic<T>
+    {
+        public void Meth1(T t)
+        {
+            T k = t;
+            Console.WriteLine(k);
+        }
+        public void Meth2(T t, bool a=true)
+        {
+            if (a)
+            {
+                Console.WriteLine(t);
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
+            
---
+++ Classwork21.03Generic2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Classwork21._03Generic2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic <int> generic = new Generic<int> {};
+            generic.Meth1(5);
+            generic.Meth2(7, false);
+        }
+    }
+}
